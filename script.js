ad829c48d90de81fdbad8805cdea56f30cb3455b
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-25 21:19:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function shortcut(s1, s2) {
   // your code here
+	let a = "";
+	let b = "";
+	if (s1.length>0){
+		a = s1[0];
+	}
+	if (s2.length>0){
+		b = s2[0];
+	}
+	if (a==="" || b===""){
+		return " ";
+	}
+	else{
+		return a+b;
+	}
+	
 }
 
 // Do not change the code below.
